48c4b85fe495cff3280133c8ebf424c65a106318
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-03-29 20:11:55 -0500

    feat(orders): refactor order handlers to use alias imports for circle depends

--- backend/modules/orders/handlers.py
+++ backend/modules/orders/handlers.py
@@ -1,49 +1,53 @@
-from modules.orders.services import get_orders, get_order_by_id
-from modules.orders.models import OrdersResponse, OrderResponse, CreateRound
-from datetime import datetime 
+from modules.orders.services import (
+    get_orders as fetch_orders,
+    get_order_by_id as fetch_order_by_id,
+    create_order as create_new_order,
+    add_round as append_round,
+    remove_round as delete_round,
+    update_round as modify_round
+)
+from modules.orders.models import OrdersResponse, OrderResponse, CreateRound, CreateOrder
+from datetime import datetime
 from fastapi import HTTPException
 
 def get_all_orders() -> OrdersResponse:
-  orders = get_orders()
-  orders_list = list(orders.values())
-  
-  orders_response = OrdersResponse(
-    last_updated=datetime.now(),
-    orders=orders_list
-  )
-  
-  return orders_response
+    orders = fetch_orders()  # Usamos el alias para evitar la recursión
+    orders_list = list(orders.values())
+
+    orders_response = OrdersResponse(
+        last_updated=datetime.now(),
+        orders=orders_list
+    )
+
+    return orders_response
+
+def post_order(order: CreateOrder) -> OrderResponse:
+    order = create_new_order(order)  # Usamos el alias
+    order_response = OrderResponse(order=order)
+    
+    return order_response
 
 def get_order_by_id(order_id: str) -> OrderResponse:
-  order = get_order_by_id(order_id)
+    order = fetch_order_by_id(order_id)  # Usamos el alias
 
-  order_response = OrderResponse(
-    order=order
-  )
-  
-  return order_response
+    order_response = OrderResponse(order=order)
+    
+    return order_response
 
 def add_round(order_id: str, round: CreateRound) -> OrderResponse:
-  
-  order = add_round(order_id, round)
-  order_response = OrderResponse(
-    order=order
-  )
-  
-  return order_response
+    order = append_round(order_id, round)  # Usamos el alias
+    order_response = OrderResponse(order=order)
+    
+    return order_response
 
 def remove_round(order_id: str, round_id: str) -> OrderResponse:
-  order = remove_round(order_id, round_id)
-  order_response = OrderResponse(
-    order=order
-  )
-  
-  return order_response
+    order = delete_round(order_id, round_id)  # Usamos el alias
+    order_response = OrderResponse(order=order)
+    
+    return order_response
 
 def update_round(order_id: str, round_id: str, round: CreateRound) -> OrderResponse:
-  order = update_round(order_id, round_id, round)
-  order_response = OrderResponse(
-    order=order
-  )
-  
-  return order_response
+    order = modify_round(order_id, round_id, round)  # Usamos el alias
+    order_response = OrderResponse(order=order)
+    
+    return order_response
