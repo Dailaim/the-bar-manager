9834bc1a9ea2e665d2f37b928256ffca130b7475
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-03-29 21:15:05 -0500

    feat(orders): add delete_orders endpoint and rename order-related functions for consistency

--- backend/modules/orders/routes.py
+++ backend/modules/orders/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from modules.orders.handlers import get_all_orders, get_order_by_id, post_order, add_round, remove_round, update_round
+from modules.orders.handlers import get_all_orders, get_order_by_id, post_order, add_round, remove_round, update_round, delete_order
 from modules.orders.models import OrdersResponse, OrderResponse, CreateRound, CreateOrder
 
 OrdersRouter = APIRouter(prefix="/orders")
@@ -14,23 +14,28 @@ def get_orders() -> JSONResponse:
 def post_orders(order: CreateOrder) -> JSONResponse:
     response = post_order(order)
     return response
+  
+@OrdersRouter.delete("/", response_model=OrderResponse)
+def delete_orders(order_id: str) -> JSONResponse:
+    response = delete_order(order_id)
+    return response
 
 @OrdersRouter.get("/{order_id}", response_model=OrderResponse)
-def get_order(order_id: str) -> JSONResponse:
+def get_orders_by_id(order_id: str) -> JSONResponse:
     response = get_order_by_id(order_id)
     return response
   
 @OrdersRouter.post("/{order_id}/rounds", response_model=OrderResponse)
-def post_round(order_id: str, _round: CreateRound) -> JSONResponse:
+def post_rounds(order_id: str, _round: CreateRound) -> JSONResponse:
     response = add_round(order_id, _round)
     return response
   
 @OrdersRouter.delete("/{order_id}/rounds/{round_id}", response_model=OrderResponse)
-def delete_round(order_id: str, round_id: str) -> JSONResponse:
+def delete_rounds(order_id: str, round_id: str) -> JSONResponse:
     response = remove_round(order_id, round_id)
     return response
   
 @OrdersRouter.put("/{order_id}/rounds/{round_id}", response_model=OrderResponse)
-def put_round(order_id: str, round_id: str, round: CreateRound) -> JSONResponse:
+def put_rounds(order_id: str, round_id: str, round: CreateRound) -> JSONResponse:
     response = update_round(order_id, round_id, round)
     return response
