6addbf850bda8599775b6a427452bb87d4f0dfc0
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-03-29 19:18:34 -0500

    feat: add OrdersRouter to main application

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
 from modules.stock.routes import StockRouter
+from modules.orders.routes import OrdersRouter
+
 app = FastAPI(title="The Bar Manager", version="0.1.0")
 app.include_router(StockRouter)
+app.include_router(OrdersRouter)
 
 @app.get("/health")
 def health_check():
