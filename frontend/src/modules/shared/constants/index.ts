4e3dae12ff889bdd3ebc6ce1b1052a8fa9c0aa36
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-03-31 01:48:23 -0500

    feat(constants): set default API_URL to localhost if environment variable is not set

--- frontend/src/modules/shared/constants/index.ts
+++ frontend/src/modules/shared/constants/index.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
 
 if (!API_URL){
   throw new Error("NEXT_PUBLIC_API_URL is not set");
